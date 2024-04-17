ec056713bda21c59c63ed1e398ff357245481faa
Mees van Dongen <meesvandongen@gmail.com>
2024-04-17 22:07:02 +0200

    add meta description

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -6,5 +6,9 @@ export default defineConfig({
   html: {
     title: "Anime Table",
     favicon: "./src/assets/ayaya.png",
+    meta: {
+      description:
+        "A table containing all Anime from the MyAnimeList API, easily filterable and sortable.",
+    },
   },
 });
